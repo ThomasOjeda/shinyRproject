e5b148a52b509d5fb745cbc920a5d818f0757ae3
Thomas Ojeda <tojeda@alumnos.exa.unicen.edu.ar>
2022-11-03 12:07:22 -0300

    commit de control

--- createMigrationPieChart.R
+++ createMigrationPieChart.R
@@ -1,5 +1,5 @@
 createMigrationPieChart = function (migrationTable) {
   dataForPie = migrationTable
   dataForPie = dataForPie[dataForPie[,2] > 0,]
-  return(pie(dataForPie[,2], labels = paste(round(prop.table(dataForPie[,2])*100), "%", dataForPie[,1], sep = "")))
+  return(pie(dataForPie[,2], labels = paste(round(prop.table(dataForPie[,2])*100), "% ", dataForPie[,1], sep = "")))
 }
