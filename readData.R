c91d151539679490ae37976ccc80ed0b78ecdeab
Thomas Ojeda <tojeda@alumnos.exa.unicen.edu.ar>
2022-11-11 19:53:37 -0300

    primera aproximacion a generacion de informacion de movimiento de estudiantes, con estudiantes que se rematricularon, que dejaron de matricularse, o que se matricularon en otra oferta academica

--- readData.R
+++ readData.R
@@ -1,23 +1,29 @@
 
-#Cargar todas las hojas del documento 
 
-sheetNames = readxl::excel_sheets("data.xlsx")
 
-yearlyData = lapply(sheetNames, function(x) 
+readData = function (filename) {
+  #Cargar todas las hojas del documento 
   
-  return (readxl::read_excel("data.xlsx", sheet=x))
-)
+  sheetNames = readxl::excel_sheets("data.xlsx")
+  
+  yearlyData = lapply(sheetNames, function(x) 
+    
+    return (readxl::read_excel("data.xlsx", sheet=x))
+  )
+  
+  yearlyDataSize = length(yearlyData)
+  
+  for (i in 1:yearlyDataSize){
+    colnames(yearlyData[[i]])[4] = "TIPO DOC"
+    colnames(yearlyData[[i]])[8] = "TIPO INS"
+    yearlyData[[i]]$DOCUMENTO = as.character(yearlyData[[i]]$DOCUMENTO)
+    
+  }
+  
+  saveRDS(yearlyData, file= "yearlyData.RDS")
+  
+}
 
-yearlyDataSize = length(yearlyData)
-
-for (i in 1:yearlyDataSize)
-yearlyData[[i]]$DOCUMENTO = as.character(yearlyData[[i]]$DOCUMENTO)
 
 
-yearlyData[[1]] %>% inner_join(yearlyData[[2]],by=c("DOCUMENTO","CARRERA")) #Esto esta bien, pero acordarse que 
-#tambien se debe ver que el tipo del segundo dataset sea reinscripto, hay gente que se inscribe 1 año y al año siguiente de nuevo.
-#Si no hago ese filtrado entonces tomo como que una segunda inscripcion equivale a una rematriculacion
-
-
-yearlyData[[1]] %>% count(DOCUMENTO)
 
