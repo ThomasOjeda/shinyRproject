33c58f74597c66b26352473f1be1015c2f4a3e63
Thomas Ojeda <tojeda@alumnos.exa.unicen.edu.ar>
2022-12-28 13:27:48 -0300

    Eliminadas funciones comentadas, funciones de prueba y funciones no utilizadas en producto final. Eliminados scripts , archivos rds y excel no utilizados en producto final. Limpieza general

--- ui.R
+++ ui.R
@@ -30,55 +30,8 @@ ui = navbarPage("Movimientos entre ofertas academicas",
                      mainPanel(
                        plotOutput("general_student_movement_ratios_pie"),
                        tableOutput("general_student_movement_ratios_info"),
-                       #plotOutput("general_student_movement_ratios_pie_TEST"),
-                       #tableOutput("general_student_movement_ratios_info_TEST")
-                       #,plotOutput("general_student_movement_ratios_simple_M")
-                       #,plotOutput("general_student_movement_ratios_simple_F")
 
                      )
                    )
-                   
           )
-          
-          # 
-          # ,
-          # tabPanel("Migraciones",
-          #          sidebarLayout(
-          #            sidebarPanel(
-          #              uiOutput("migrations_unit_selector"),
-          #              helpText("Seleccione un rango de años"),
-          #              uiOutput("migration_range_selectors") ,
-          #              sliderInput("delta", 
-          #                          label = "Delta",
-          #                          min = 0, max = 10, value = 0)
-          #            ),
-          #            
-          #            mainPanel( fluidPage(
-          #              h3("Informacion de migraciones"),
-          #              tableOutput("migration_data"),
-          #              h3("De las migraciones, porcentaje perteneciente a cada carrera"),
-          #              plotOutput("migration_pie_chart")                   
-          #                                 
-          #                                 ))
-          #            
-          #          )
-          #  ),
-          #  tabPanel("Historico de migraciones por carrera",
-          #           sidebarLayout(
-          #             sidebarPanel(
-          #               uiOutput("unit_hist_selector"),
-          #               uiOutput("degree_hist_selector")
-          #             ),
-          #             mainPanel(
-          #               plotOutput("migration_history", width = "80%", height = "700px")
-          #             )
-          #           )
-          #             
-          #  ),
-          #  tabPanel("Red",
-          #           plotOutput("network")
-          #  )
-          
-          
-          
 )
